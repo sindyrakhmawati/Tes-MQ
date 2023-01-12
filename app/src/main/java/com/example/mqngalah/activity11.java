132704986a2bc6ed19f3a0c4cab73a54cc1c724d
unknown <novitawsari0047@gmail.com>
2023-01-12 08:52:20 +0700

    update

---
+++ app/src/main/java/com/example/mqngalah/activity11.java
@@ -0,0 +1,14 @@
+package com.example.mqngalah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity11 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_11);
+    }
+}

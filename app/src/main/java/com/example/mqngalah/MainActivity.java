caf9956128d983a76a03f5a5c439b7101fc83267
unknown <novitawsari0047@gmail.com>
2023-01-12 08:58:14 +0700

    penambahan activity

--- app/src/main/java/com/example/mqngalah/MainActivity.java
+++ app/src/main/java/com/example/mqngalah/MainActivity.java
@@ -36,6 +36,94 @@ public class MainActivity extends AppCompatActivity {
                 openactivity3();
             }
         });
+
+        button = (Button) findViewById(R.id.hukummimsukun);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openactivity4();
+            }
+        });
+
+        button = (Button) findViewById(R.id.idghom);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openactivity5();
+            }
+        });
+
+        button = (Button) findViewById(R.id.ikhfajadid);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openactivity6();
+            }
+        });
+
+        button = (Button) findViewById(R.id.hukumlamtarif);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openactivity7();
+            }
+        });
+
+        button = (Button) findViewById(R.id.hukumlamjalalah);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openactivity8();
+            }
+        });
+
+        button = (Button) findViewById(R.id.hukumro);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openactivity9();
+            }
+        });
+
+        button = (Button) findViewById(R.id.qolqolah);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openactivity10();
+            }
+        });
+
+        button = (Button) findViewById(R.id.hukummad);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openactivity11();
+            }
+        });
+
+        button = (Button) findViewById(R.id.profil);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openprofil();
+            }
+        });
+
+        button = (Button) findViewById(R.id.sanad);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opensanad();
+            }
+        });
+
+        button = (Button) findViewById(R.id.riwayat);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openriwayat();
+            }
+        });
     }
 
     public void openactivity2(){
@@ -45,6 +133,61 @@ public class MainActivity extends AppCompatActivity {
 
     public void openactivity3(){
         Intent intent = new Intent(this, activity3.class);
+        startActivity(intent);
+    }
+
+    public void openactivity4(){
+        Intent intent = new Intent(this, activity4.class);
+        startActivity(intent);
+    }
+
+    public void openactivity5(){
+        Intent intent = new Intent(this, activity5.class);
+        startActivity(intent);
+    }
+
+    public void openactivity6(){
+        Intent intent = new Intent(this, activity6.class);
+        startActivity(intent);
+    }
+
+    public void openactivity7(){
+        Intent intent = new Intent(this, activity7.class);
+        startActivity(intent);
+    }
+
+    public void openactivity8(){
+        Intent intent = new Intent(this, activity8.class);
+        startActivity(intent);
+    }
+
+    public void openactivity9(){
+        Intent intent = new Intent(this, activity9.class);
+        startActivity(intent);
+    }
+
+    public void openactivity10(){
+        Intent intent = new Intent(this, activity10.class);
+        startActivity(intent);
+    }
+
+    public void openactivity11(){
+        Intent intent = new Intent(this, activity11.class);
+        startActivity(intent);
+    }
+
+    public void openprofil(){
+        Intent intent = new Intent(this, profil.class);
+        startActivity(intent);
+    }
+
+    public void opensanad(){
+        Intent intent = new Intent(this, sanad.class);
+        startActivity(intent);
+    }
+
+    public void openriwayat(){
+        Intent intent = new Intent(this, riwayat.class);
         startActivity(intent);
     }
 }

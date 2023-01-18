48dc42b63b7213189d7e5842965b3e297e21f807
LaelatulHidayah <120804445+LaelatulHidayah@users.noreply.github.com>
2023-01-19 06:18:31 +0700

    Update MainActivity.java
    mengganti button menjadi recyclerView

--- app/src/main/java/com/example/mqngalah/MainActivity.java
+++ app/src/main/java/com/example/mqngalah/MainActivity.java
@@ -1,216 +1,108 @@
 package com.example.mqngalah;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import java.util.ArrayList;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    RecyclerView recyclerView;
+    RecyclerView.Adapter recyclerViewAdapter;
+    RecyclerView.LayoutManager recyclerViewLayoutManager;
+    ArrayList<ItemModel> data;
+
+    private Button profilButton;
+    private Button sanadButton;
+
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.hukummimdannunbertasdid);
-        button.setOnClickListener(new View.OnClickListener() {
+        profilButton = findViewById(R.id.profil);
+        sanadButton = findViewById(R.id.sanad);
+
+        profilButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openactivity2();
+                Intent intent = new Intent(MainActivity.this, profil.class);
+                startActivity(intent);
             }
         });
 
-        button = (Button) findViewById(R.id.hukumnunsukundantanwin);
-        button.setOnClickListener(new View.OnClickListener() {
+        sanadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openactivity3();
+                Intent intent = new Intent(MainActivity.this, sanad.class);
+                startActivity(intent);
             }
         });
 
-        button = (Button) findViewById(R.id.hukummimsukun);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openactivity4();
-            }
-        });
 
-        button = (Button) findViewById(R.id.idghom);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openactivity5();
-            }
-        });
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
 
-        button = (Button) findViewById(R.id.ikhfajadid);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openactivity6();
-            }
-        });
+        recyclerViewLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(recyclerViewLayoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
 
-        button = (Button) findViewById(R.id.hukumlamtarif);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openactivity7();
-            }
-        });
+        data = new ArrayList<>();
+        for (int i = 0; i < myitem.Headline.length; i ++) {
+            data.add(new ItemModel(
+                    myitem.Headline[i],
+                    myitem.Subheadline[i],
+                    myitem.iconlist[i]));
+        }
 
-        button = (Button) findViewById(R.id.hukumlamjalalah);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openactivity8();
-            }
-        });
-
-        button = (Button) findViewById(R.id.hukumro);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openactivity9();
-            }
-        });
-
-        button = (Button) findViewById(R.id.qolqolah);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openactivity10();
-            }
-        });
-
-        button = (Button) findViewById(R.id.hukummad);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openactivity11();
-            }
-        });
-
-        button = (Button) findViewById(R.id.profil);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openprofil();
-            }
-        });
-
-        button = (Button) findViewById(R.id.sanad);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                opensanad();
-            }
-        });
+        recyclerViewAdapter = new AdapterRecyclerView(this, data);
+        recyclerView.setAdapter(recyclerViewAdapter);
     }
-
-    public void openactivity2(){
-        Intent intent = new Intent(this, activity2.class);
-        startActivity(intent);
-    }
-
-    public void openactivity3(){
-        Intent intent = new Intent(this, activity3.class);
-        startActivity(intent);
-    }
-
-    public void openactivity4(){
-        Intent intent = new Intent(this, activity4.class);
-        startActivity(intent);
-    }
-
-    public void openactivity5(){
-        Intent intent = new Intent(this, activity5.class);
-        startActivity(intent);
-    }
-
-    public void openactivity6(){
-        Intent intent = new Intent(this, activity6.class);
-        startActivity(intent);
-    }
-
-    public void openactivity7(){
-        Intent intent = new Intent(this, activity7.class);
-        startActivity(intent);
-    }
-
-    public void openactivity8(){
-        Intent intent = new Intent(this, activity8.class);
-        startActivity(intent);
-    }
-
-    public void openactivity9(){
-        Intent intent = new Intent(this, activity9.class);
-        startActivity(intent);
-    }
-
-    public void openactivity10(){
-        Intent intent = new Intent(this, activity10.class);
-        startActivity(intent);
-    }
-
-    public void openactivity11(){
-        Intent intent = new Intent(this, activity11.class);
-        startActivity(intent);
-    }
-
-    public void openprofil(){
-        Intent intent = new Intent(this, profil.class);
-        startActivity(intent);
-    }
-
-    public void opensanad(){
-        Intent intent = new Intent(this, sanad.class);
-        startActivity(intent);
-    }
-    
     public void play(View v) {
         if (player == null) {
             player = MediaPlayer.create(this, R.raw.makhrotan);
             player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                 @Override
-                public void onCompletion(MediaPlayer mediaPlayer) {
+                public void onCompletion(MediaPlayer mp) {
                     stopPlayer();
                 }
             });
+
         }
         player.start();
     }
-    
-    public void stop(View v) {
-        stopPlayer();
-    }
-    
+
     public void pause(View v) {
-        if (player != null) {
+        if (player != null){
             player.pause();
         }
     }
-    private  void  stopPlayer(){
-        if (player != null) {
-            player.release();
-            player = null;
-            Toast.makeText(this, "MediaPlayer Stop", Toast.LENGTH_SHORT).show();
-        }
+
+    public void stop(View v) {
+        stopPlayer();
     }
 
+    private void stopPlayer() {
+        if (player != null){
+            player.release();
+            player =null;
+        }
+    }
     @Override
-    protected void onStop() {
+    protected void onStop(){
         super.onStop();
         stopPlayer();
     }
+
 }

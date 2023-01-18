5a922a98a12890119c1923bc6ca7717df11f462c
LaelatulHidayah <120804445+LaelatulHidayah@users.noreply.github.com>
2023-01-19 06:25:42 +0700

    Add files via upload
    komponen RecyclerView

---
+++ app/src/main/java/com/example/mqngalah/myitem.java
@@ -0,0 +1,16 @@
+package com.ella.reycicle;
+
+public class myitem {
+
+    static int[] iconlist = {
+            R.drawable.satu, R.drawable.dua, R.drawable.tiga, R.drawable.empat, R.drawable.lima, R.drawable.enam, R.drawable.tuju, R.drawable.delapan, R.drawable.sembilan, R.drawable.sepuluh
+    };
+
+    static String[] Headline = {
+            "Hukum Mim dan Nun Bertasyid", "Hukum Nun Sukun dan Tanwin", "Hukum Mim Sukun", "Idghom", "Ikhfa' Jadid", "Hukum Lam Ta'rif", "Hukum Lam Jalalah", "Hukum Ro'", "Qolqolah", "Hukum Mad"
+    };
+
+    static String[] Subheadline = {
+            "Selengkapnya", "Selengkapnya","Selengkapnya","Selengkapnya","Selengkapnya","Selengkapnya","Selengkapnya","Selengkapnya","Selengkapnya","Selengkapnya"
+    };
+}
